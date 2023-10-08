e3dd4621258c8b055b744fced23df6d20f1b7510
dogangle <51759064+dogangle@users.noreply.github.com>
2023-10-07 23:09:45 -0400

    real original

--- src/App.vue
+++ src/App.vue
@@ -130,7 +130,7 @@ export default {
           "mech": "Synecdoche"
         },
         {
-          "callsign": "Chewy",
+          "callsign": "Rodeo",
           "alias": "Ernest Bubblebottom",
           "code": "4be26ce9-923b-4069-b6c9-76437d4be455///NDL-C-OFFICE-ROYALTY//056940c6-8d55-4190-8e85-57caa043cb1a",
           "corpro": "IPS-N",
