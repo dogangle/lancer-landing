d6454e9a8e37e2894d02c864d72591ed670f65bc
dogangle <51759064+dogangle@users.noreply.github.com>
2024-04-21 13:41:06 -0400

    update mission

--- src/App.vue
+++ src/App.vue
@@ -89,14 +89,19 @@ export default {
 
   data() {
     return {
-      "mission_slug": "002",
+      "mission_slug": "003",
       "current_md": "",
       "events": "",
       "missions": [
         {
+          "slug": "003",
+          "name": "Operation Calling Card",
+          "status": "start"
+        },
+        {
           "slug": "002",
           "name": "The Battle for Solo Terra",
-          "status": "start"
+          "status": "success"
         },
         {
           "slug": "001",
